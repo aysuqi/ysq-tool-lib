09cc882cd93c8ad80809ce19122e9aa8b258386b
yusunqi <ysuqi01@126.com>
2023-09-17 10:58:06 +0800

    feat: 搭建基础的npm 包插件框架

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,8 @@
  * @Description:
  * @Author: yusunqi
  * @Date: 2023-09-16 14:18:59
- * @LastEditTime: 2023-09-16 23:39:43
+ * @LastEditTime: 2023-09-17 10:54:24
  * Copyright (c) 2023 by yusunqi, All Rights Reserved.
  */
-import * as core from './core'
 
 export * from './core'
-
-const Ys = { format: core.format }
-
-if (typeof window === 'undefined') {
-	throw new Error('this is no window ENV')
-}
-
-export default Ys
